085e6237563441ab47c9ebcee11299498128fde6
Seth Dousman <sdousman3@gmail.com>
2019-10-01 13:55:23 -0400

    able to add gifs to page dynamically. adding style

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,5 +1,8 @@
 let buttons = ['cats', 'dogs', 'lions','test'];
 
+const apiKey = 'YcXH84Jw7UjWmn3aixZ7TS9mhScDELxP';
+const endPoint = 'http://api.giphy.com/v1/gifs/search?apikey=YcXH84Jw7UjWmn3aixZ7TS9mhScDELxP';
+
 function loadButtons() {
     const listButtons = JSON.parse( localStorage.getItem('buttons') );
 
@@ -21,6 +24,7 @@ function renderButtons() {
             >${buttonName}</button>
             <button
                 data-name="${buttonName}"
+                data-index="${i}"
                 class="btn btn-delete fas fa-times"
             ></button>
         </div>
@@ -37,17 +41,117 @@ localStorage.setItem('buttons',JSON.stringify(buttons));
 loadButtons();
 renderButtons();
 
-$('#submit-button').on('click',function (event) {
+
+
+
+
+function removeButton(){
+
+    const buttonIndex = $(this).attr('data-index');
+
+    buttons.splice(buttonIndex,1);
+
+    renderButtons();
+
+}
+
+function addButton(value){
+    buttons.push(value);
+    renderButtons();
+}
+
+
+
+function renderGiphys(giphys){
+    $('.giphy-content').empty();
+
+    for(let i = 0; i < giphys.length; i++){
+        const giphy = giphys[i];  
+
+        
+    
+        const giphyTemplate = createTemplate(giphy);
+
+  $('.giphy-content').append(giphyTemplate);
+    }
+
+
+}
+
+function createTemplate(giphy){
+    const images = giphy.images;
+    const template =` 
+    <div class="giphy">
+        <i class="far fa-star favorite" data-id="${giphy.id}" data-star="false">
+        </i>
+        <div class="giphy-image">
+             <img 
+                src="${images.original_still.url}"
+                data-still="${images.original_still.url}"
+                data-animate="${images.original.url}" data-state="still">
+            <i class="fa fa-play img-play"></i>
+</div>
+<div class="giphy-info">
+    <p>Rating: g</p>
+    <p>Posted A Year Ago</p>
+</div>
+
+<div class="giphy-footer" data-link="${giphy.embed_url}"> 
+    <p>Copy Link <i class="fa fa-link"></i></p>
+</div>
+</div> 
+`;
+
+return template;
+
+
+
+
+
+}
+
+
+
+
+function getGiphy(value) {
+
+    const url = endPoint + '&q=' + value;
+    
+    $.ajax({url})
+
+    .then(function(response) {
+        const giphys = response.data;
+
+        renderGiphys(giphys);
+        console.log('Giphys:',giphys)
+    })
+    .catch(function(error) {
+        console.log('Error:',error);
+
+    });
+    
+}
+
+
+
+function searchGiphy(event){
     event.preventDefault();
+    const value = $('#search').val();
+    addButton(value);
+    getGiphy(value);
 
-const value = $('#search').val();
+    
 
-buttons.push(value);
 
-renderButtons();
 
-console.log('Value', value);
+}
 
-});
+
+
+///events
+$(document).on('click','.btn-delete',removeButton);
+$('#submit-button').on('click',searchGiphy);
+    
+
 
 
